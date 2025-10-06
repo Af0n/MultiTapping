b2b00d38116943cfbb11e619683e487882aeb663
afonc <37196107+Af0n@users.noreply.github.com>
2025-10-05 21:09:08 -0700

    Starting on Bonus Region

--- Assets/Scripts/SpaceSlider.cs
+++ Assets/Scripts/SpaceSlider.cs
@@ -4,22 +4,43 @@ public class SpaceSlider : MonoBehaviour
 {
     public float MinX;
     public float MaxX;
+
     public float Rate;
     public bool IsReverse;
 
-    private float _t;
+    public float BonusSize = 0.9f;
+
+    private Transform _ball;
+    private Transform _bonusRegion;
+    private float _t = 0.5f;
 
     public float XPos
     {
         get { return MinX + (MaxX - MinX) * _t; }
     }
 
-    public void ToggleReverse() {
-        IsReverse = !IsReverse;
+    private void Awake()
+    {
+        _ball = transform.GetChild(0);
+        _ball = transform.GetChild(1);
+    }
+
+    void Start()
+    {
+        MinX = CameraBounds.instance.MinX;
+        MaxX = CameraBounds.instance.MaxX;
     }
 
     public void Update()
     {
+        // changing the interpolation value;
         _t += IsReverse ? -Rate * Time.deltaTime : Rate * Time.deltaTime;
+        // updating ball position
+        _ball.localPosition = new(XPos, 0, 0);
+    }
+
+    public void ToggleReverse()
+    {
+        IsReverse = !IsReverse;
     }
 }
